29beebc1728c95f2141071cd578b2c62766b4260
Gustavo Morel <gustavo.d.morel@gmail.com>
2021-10-24 14:23:34 -0300

    counter n

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import {Button, Card} from 'react-bootstrap';
 
+
+
 function Counter() {
   
   const [count, setCount] = useState(0);
 
   let titulo = "Torta de Cumple"
+  let stock = 10
+
+ function Stock (){
+  if ([count]>stock){
+    alert("No se cuenta con stock disponible.")
+  } else {alert("Compra confirmada")}
+  
+}
 
   return (
 
   <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="" />
+    <Card.Img variant="top" src="../../public/assets/menu/tortaCumpleanos.jpg" />
     <Card.Body>
         <Card.Title>{titulo}</Card.Title>
         <Card.Text>
@@ -19,20 +29,11 @@ function Counter() {
       <Button  onClick={() => setCount(count + 1)} variant="success">+</Button>
       <Button onClick={() => setCount(count - 1)} variant="danger">-</Button>
       <Card.Footer>
-      <Button>Confirmar Compra</Button>   
+      <Button onClick={(Stock)}>Confirmar Compra</Button>   
       </Card.Footer>
     </Card.Body>
   </Card>
 
-    /* <div>
-      <p>Veces seleccionado {count} </p>
-      <Button  onClick={() => setCount(count + 1)} variant="success">
-        +
-      </Button>
-      <Button onClick={() => setCount(count - 1)} variant="danger">
-        -
-      </Button>
-    </div> */
 
   );
 }
